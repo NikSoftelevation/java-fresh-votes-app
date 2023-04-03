5afdb671932feebdeab1baa6468417b24d9dbc24
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-04-03 14:04:02 +0530

    Imported created project and done some initial configurations

---
+++ src/test/java/com/freshvotes/freshvotes/FreshVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshvotes.freshvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
