1209e09739609925581fe24b6e732cf34c6425a9
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-04-10 13:09:20 +0530

    Retrieving Productt Data from DB

--- src/main/java/com/freshvotes/freshvotes/controller/ProductController.java
+++ src/main/java/com/freshvotes/freshvotes/controller/ProductController.java
@@ -2,8 +2,11 @@ package com.freshvotes.freshvotes.controller;
 
 import com.freshvotes.freshvotes.domain.Product;
 import com.freshvotes.freshvotes.domain.User;
+import com.freshvotes.freshvotes.repository.ProductRepository;
 import com.freshvotes.freshvotes.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,10 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
+import java.util.Optional;
+
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
 
     @GetMapping("/products")
     public String getProducts(ModelMap model) {
@@ -22,7 +30,16 @@ public class ProductController {
     }
 
     @GetMapping("/products/{productId}")
-    public String getProduct(@PathVariable("productId") Long productId) {
+    public String getProduct(@PathVariable("productId") Long productId, HttpServletResponse response, ModelMap model) throws IOException {
+        Optional<Product> productOpt = productRepository.findById(productId);
+
+        if (productOpt.isPresent()) {
+            Product product = productOpt.get();
+
+            model.put("product", product);
+        } else {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id : " + productId + "was not found");
+        }
         return "product";
     }
 
